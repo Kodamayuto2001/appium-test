b1537a4a88bb08e8fbfc13a16da46a25f26f8d92
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2022-02-06 17:26:00 +0900

    add sample program

---
+++ src/test/java/appium/test/NotepadTest.java
@@ -0,0 +1,50 @@
+package appium.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.windows.WindowsDriver;
+
+public class NotepadTest {
+	public static WindowsDriver driver = null;
+
+	@BeforeClass
+	public void setUp(){
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("app", "\"C:\\Windows\\System32\\notepad.exe\"");
+		cap.setCapability("platformName", "Windows");
+		cap.setCapability("deviceName", "WindowsPC");
+
+		try {
+			driver = new WindowsDriver(new URL("http://127.0.0.1:4723"), cap);
+		} catch (MalformedURLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+	@AfterMethod
+	public void cleanUp() {
+		driver.quit();
+		setUp();
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void checkHelpAboutNowTest() {
+		driver.findElementByName("ヘルプ(H)").click();
+	}
+}
